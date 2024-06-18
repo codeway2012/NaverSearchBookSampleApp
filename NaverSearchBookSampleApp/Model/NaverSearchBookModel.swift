03873f92d393003c0bf8fd981fcfd21b41e6126c
youngwoo_ahn <codeway119@gmail.com>
2024-06-18 21:43:21 +0900

    rest api

--- NaverSearchBookSampleApp/Model/NaverSearchBookModel.swift
+++ NaverSearchBookSampleApp/Model/NaverSearchBookModel.swift
@@ -4,15 +4,25 @@
 //
 //  Created by user on 6/18/24.
 //
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let naverSearchBook = try? JSONDecoder().decode(NaverSearchBook.self, from: jsonData)
 
-import SwiftUI
+import Foundation
 
-struct NaverSearchBookModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+// MARK: - NaverSearchBook
+struct NaverSearchBook: Codable {
+	let lastBuildDate: String
+	let total, start, display: Int
+	let items: [Item]
 }
 
-#Preview {
-    NaverSearchBookModel()
+// MARK: - Item
+struct Item: Codable {
+	let title: String
+	let link: String
+	let image: String
+	let author, discount, publisher, pubdate: String
+	let isbn, description: String
 }
--- NaverSearchBookSampleApp/Model/NetworkAPI.swift
+++ NaverSearchBookSampleApp/Model/NetworkAPI.swift
@@ -5,14 +5,83 @@
 //  Created by user on 6/18/24.
 //
 
-import SwiftUI
+import Foundation
 
-struct NetworkAPI: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+
+func searchBooks(query: String) async throws -> [Book] {
+	guard let url = URL(string: "\(baseURL)?query=\(query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")") else {
+		throw NSError(domain: "Invalid URL", code: 0, userInfo: nil)
+	}
+	
+	var request = URLRequest(url: url)
+	request.httpMethod = "GET"
+	request.addValue(clientID, forHTTPHeaderField: "X-Naver-Client-Id")
+	request.addValue(clientSecret, forHTTPHeaderField: "X-Naver-Client-Secret")
+	
+	let (data, response) = try await URLSession.shared.data(for: request)
+	
+	guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+		throw NSError(domain: "HTTP Error", code: (response as? HTTPURLResponse)?.statusCode ?? 0, userInfo: nil)
+	}
+	
+	let bookResponse = try JSONDecoder().decode(BookResponse.self, from: data)
+	return bookResponse.items
 }
 
-#Preview {
-    NetworkAPI()
-}
+
+
+//Task {
+//	do {
+//		let books = try await searchBooks(query: "swift")
+//		print("Books: \(books)")
+//	} catch {
+//		print("Error: \(error.localizedDescription)")
+//	}
+//}
+
+
+
+
+
+
+//
+//
+//func searchBooks(query: String, completion: @escaping ([Book]?, Error?) -> Void) {
+//	guard let url = URL(string: "\(baseURL)?query=\(query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")") else {
+//		completion(nil, NSError(domain: "Invalid URL", code: 0, userInfo: nil))
+//		return
+//	}
+//	
+//	let clientID = "P30m8bv4KbaW9fC3jeCy"
+//	let clientSecret = "245CRfCnfa"
+//	
+//	var request = URLRequest(url: url)
+//	request.httpMethod = "GET"
+//	request.addValue(clientID, forHTTPHeaderField: "X-Naver-Client-Id")
+//	request.addValue(clientSecret, forHTTPHeaderField: "X-Naver-Client-Secret")
+//	
+//	URLSession.shared.dataTask(with: request) { data, response, error in
+//		if let error = error {
+//			completion(nil, error)
+//			return
+//		}
+//		
+//		guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+//			completion(nil, NSError(domain: "HTTP Error", code: (response as? HTTPURLResponse)?.statusCode ?? 0, userInfo: nil))
+//			return
+//		}
+//		
+//		guard let data = data else {
+//			completion(nil, NSError(domain: "No data received", code: 0, userInfo: nil))
+//			return
+//		}
+//		
+//		do {
+//			let naverSearchBook = 
+//			try? JSONDecoder().decode(NaverSearchBook.self, from: data)
+//			completion(naverSearchBook.items, nil)
+//		} catch {
+//			completion(nil, error)
+//		}
+//	}.resume()
+//}
