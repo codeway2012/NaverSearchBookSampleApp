c36c73f1964af3b7758f49097a46e45886adbecb
youngwoo_ahn <codeway119@gmail.com>
2024-06-18 21:43:11 +0900

    rest api

---
+++ NaverSearchBookSampleApp/Model/NaverSearchBookModel.swift
@@ -0,0 +1,18 @@
+//
+//  NaverSearchBookModel.swift
+//  NaverSearchBookSampleApp
+//
+//  Created by user on 6/18/24.
+//
+
+import SwiftUI
+
+struct NaverSearchBookModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NaverSearchBookModel()
+}
---
+++ NaverSearchBookSampleApp/Model/NetworkAPI.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkAPI.swift
+//  NaverSearchBookSampleApp
+//
+//  Created by user on 6/18/24.
+//
+
+import SwiftUI
+
+struct NetworkAPI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetworkAPI()
+}
